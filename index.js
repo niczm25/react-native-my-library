757281e3706d7826428349d516b980f80246a680
Neil <neil.mosca@lenddo.com>
2018-12-20 22:39:31 +0800

    Rename to react-native-my-library

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { NativeModules } from 'react-native';
 
-const { RNReactNativeMyLibrary } = NativeModules;
+const { RNMyLibrary } = NativeModules;
 
-export default RNReactNativeMyLibrary;
+export default RNMyLibrary;
